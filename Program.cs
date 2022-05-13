d91ce58a66fbdfbcc4fc3f87baed1e750b466e80
Daniel Mackay <daniel.mackay@levo.com.au>
2022-05-13 21:53:50 +1000

    Added in Serilog

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorHelloWorld;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.BrowserConsole()
+    .CreateLogger();
 
 await builder.Build().RunAsync();
