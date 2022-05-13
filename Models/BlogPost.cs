2a646792eba7e0fa5a8b65ef93e43fc244fb0af0
Daniel Mackay <daniel.mackay@levo.com.au>
2022-05-13 22:52:48 +1000

    Added fluent validation

---
+++ Models/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorHelloWorld.Models
+{
+    public class BlogPost
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public string Slug { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+
+    public class BlogPostValidator : AbstractValidator<BlogPost>
+    {
+        public BlogPostValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Slug).NotEmpty().Length(3,16);
+            RuleFor(x => x.Content).NotEmpty();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# BlazorHelloWorld
+# Blazor Hello World
+
+## Overview
+
+Demo Blazor application that includes the following:
+- Counter button
+- Fetching weather data
+- Logging to the browser console via Serilog
+- Inline field validation via Fluent Validation
+
+## Resources
+Based off of https://youtu.be/Oeh2IJw7Zig
