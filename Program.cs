cd6db5b31a0d3fe25105b46b81343e344106f17f
Daniel Mackay <daniel.mackay@levo.com.au>
2022-05-14 17:22:57 +1000

    Added markdown to HTML renderer

--- Program.cs
+++ Program.cs
@@ -2,12 +2,20 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorHelloWorld;
 using Serilog;
+using Markdig;
+using Markdig.SyntaxHighlighting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<MarkdownPipeline>(sp =>
+    new MarkdownPipelineBuilder()
+    .UseAdvancedExtensions()
+    .UseSyntaxHighlighting()
+    .Build());
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
